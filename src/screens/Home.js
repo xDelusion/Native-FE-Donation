dfb2117b360e1e52b22d4d485958710fe8c56692
methreban2023 <alrefai.dalia@gmail.com>
2023-08-12 23:58:52 +0300

    working on blood type statistics

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,46 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getRecipientReqs } from "../apis/recipientRequest/recipient";
+import React, { useEffect } from "react";
+import Chart from "../components/Chart/Chart";
 
 const Home = ({ navigation }) => {
+  const {
+    data: recipientRequestData,
+    isFetching,
+    refetch,
+  } = useQuery({
+    queryKey: ["recipientRequest"],
+    queryFn: () => getRecipientReqs(),
+    onSuccess: (data) => {
+      console.log(`data = ${data}`);
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+  let bloodTypeCounts = {};
+  if (recipientRequestData) {
+    recipientRequestData.forEach((recipientRequest) => {
+      if (bloodTypeCounts[recipientRequest.bloodType]) {
+        bloodTypeCounts[recipientRequest.bloodType] += 1;
+      } else {
+        bloodTypeCounts[recipientRequest.bloodType] = 1;
+      }
+    });
+  } else {
+    console.log("no data");
+  }
+  const bloodTypeData = Object.keys(bloodTypeCounts).map((bloodType) => ({
+    label: bloodType,
+    value: bloodTypeCounts[bloodType],
+  }));
+
   return (
     <View>
+      <View>
+        <Chart bloodTypes={bloodTypeData} />
+      </View>
       <Text>Home</Text>
     </View>
   );
