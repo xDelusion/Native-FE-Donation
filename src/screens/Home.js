1471074d50422973474ddd2778e30d5281fbc9fc
methreban2023 <alrefai.dalia@gmail.com>
2023-08-13 18:04:42 +0300

    home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -9,93 +9,21 @@ import ROUTES from "../navigation/routes";
 // import Chart from "../components/HomeRecipientRequest/Chart";
 
 const Home = ({ navigation }) => {
-  const [bloodType, setBloodType] = useState("ALL");
-  const [focusedBloodType, setFocusedBloodType] = useState("ALL");
-  const bloodArray = ["O+", "O-", "A+", "A-", "B+", "B-", "AB+", "AB-", "ALL"];
-
-  const {
-    data: recipientRequestData,
-    isFetching,
-    refetch,
-  } = useQuery({
-    queryKey: ["recipientRequest"],
-    queryFn: () => getRecipientReqs(),
-    onSuccess: (data) => {
-      console.log(`data = ${data}`);
-    },
-    onError: (error) => {
-      console.log(error);
-    },
-  });
-
-  const handleBloodTypePress = (selectedBloodType) => {
-    if (selectedBloodType !== "ALL") {
-      setBloodType(selectedBloodType);
-
-      setFocusedBloodType(selectedBloodType);
-    }
-  };
-
-  const filterData = recipientRequestData?.map((recipientRequest) => {
-    if (bloodType === "ALL" || bloodType === recipientRequest.bloodType) {
-      return (
-        <RecipientRequestItem
-          key={recipientRequest.serial_no}
-          request={recipientRequest}
-        />
-      );
-    }
-    return null;
-  });
-
   return (
-    <ScrollView style={styles.container}>
-      <View
-        style={[
-          styles.headerContainer,
-          { flexDirection: "row", justifyContent: "space-between" },
-        ]}
-      >
-        <Text>Home</Text>
-        <Button
-          title="Statistics"
-          onPress={() => {
-            navigation.navigate(ROUTES.APPROUTES.STATISTICS);
-          }}
-        />
-      </View>
-      <View style={styles.section}>
-        <Text>Blood Bags Required</Text>
-        {/* <View>
-            <Chart bloodTypes={bloodTypeData} />
-          </View> */}
-      </View>
-      <View style={styles.contentContainer}>
-        <View style={styles.section}>
-          <Text>Recipient Requests</Text>
-        </View>
-        <View style={styles.section}>{filterData}</View>
-        <View style={styles.section}>
-          <Text>Blood Groups</Text>
-          <ScrollView
-            contentContainerStyle={styles.bloodGroupContainer}
-            horizontal={false} // Allow horizontal scrolling
-          >
-            {bloodArray.map((bloodType) => (
-              <BloodTypeButton
-                key={bloodType}
-                bloodType={bloodType}
-                focused={focusedBloodType === bloodType}
-                onFocus={(selectedBloodType) =>
-                  setFocusedBloodType(selectedBloodType)
-                }
-                onPress={() => handleBloodTypePress(bloodType)}
-              />
-            ))}
-          </ScrollView>
-        </View>
-      </View>
-    </ScrollView>
+    <View
+      style={[
+        styles.headerContainer,
+        { flexDirection: "row", justifyContent: "space-between" },
+      ]}
+    >
+      <Text>Home</Text>
+      <Button
+        title="Statistics"
+        onPress={() => {
+          navigation.navigate(ROUTES.APPROUTES.STATISTICS);
+        }}
+      />
+    </View>
   );
 };
 
